932ca0d0eb17178d0b87ea889ca39903f27e1c0d
Alexandre Nascimento <81328399+Alexandresfi@users.noreply.github.com>
2021-07-30 13:31:21 -0300

    Update Cadastrar.js

--- src/Cadastrar/Cadastrar.js
+++ src/Cadastrar/Cadastrar.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 import {AreaCadastro} from "./styled"
 import Header from "../Header/Header"
 
 let Teste = [];
-let pos = 0;
+
 function Cadastrar(){
 
 	const [nome,setNome] = useState("");
@@ -21,9 +21,21 @@ function Cadastrar(){
 			Teste=[...alunos, nome];
 			setNome("");
 		}
+		
 		console.log(Teste);
 		console.log(alunos);
 	}
+
+	useEffect(()=>{
+		let savedItems = JSON.parse(localStorage.getItem("savedItems"))
+		if(savedItems){
+			setAlunos(savedItems)
+		}
+	},[])
+
+	useEffect(()=>{
+		localStorage.setItem("savedItems", JSON.stringify(alunos))
+	},[alunos])
 
 	return(
 		<>
@@ -34,6 +46,7 @@ function Cadastrar(){
 			Dados do estudante:
 		</label> <br/>
 		<input onChange={Alterando} type="text" name="nome de estudante" placeholder="Nome do estudante" value={nome} autoFocus></input>
+		<input type="number" name="ano" placeholder="Ano"/> <br/>
 		<input type="text" name="turma" placeholder="Turma"/>
 		<input type="email" name="email" placeholder="E-mail do responsável"/> <br/>
 
@@ -43,13 +56,14 @@ function Cadastrar(){
 		</label> <br/>
 		<input type="text" name="nome da mãe" placeholder="Nome da mãe"/>
 				<input type="text" name="nome do pai" placeholder="Nome do pai"/> <br/>
-				<input type="text" name="celular" placeholder="Numero do celular"/>
-				<input type="text" name="celular 2" placeholder="Numero do celular"/><br/>
+				<input type="text" name="celular" placeholder="Número do celular"/>
+				<input type="text" name="celular 2" placeholder="Número do celular"/><br/>
 
 		<label>
 			Dados residenciais dos responsáveis:
 		</label><br/>
-		<input type="text" name="Endereço" placeholder="Endereço"/>
+		<input type="number" name="CEP" placeholder="CEP"/>
+		<input type="text" name="Endereço" placeholder="Endereço"/> <br/>
 		<input type="number" name="número da casa" placeholder="Nº"/> <input type="text" name="complemento"
 		placeholder="complemento"/> <br/>
 		<input type="text" name="Cidade" placeholder="Cidade"/>
