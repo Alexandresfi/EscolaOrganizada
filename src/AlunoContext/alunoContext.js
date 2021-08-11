ee9cb1926b9009e9d3d5e813962dbb552e1857d2
Alexandre Nascimento <81328399+Alexandresfi@users.noreply.github.com>
2021-08-11 14:07:34 -0300

    Add files via upload

---
+++ src/AlunoContext/alunoContext.js
@@ -0,0 +1,13 @@
+import React, {useState, createContext} from "react";
+
+export const AlunoContext = createContext();
+
+export function AlunoProvider(props) {
+    const [alunos,setAlunos] = useState([]);
+
+    return(
+        <AlunoContext.Provider value={[alunos,setAlunos]}>
+            {props.children}
+        </AlunoContext.Provider>
+    )
+}
