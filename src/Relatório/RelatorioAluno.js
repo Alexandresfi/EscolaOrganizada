fbac003d70dc143926c87983e5d56f68cd5e4523
Alexandre Nascimento <81328399+Alexandresfi@users.noreply.github.com>
2021-08-11 14:06:03 -0300

    Update RelatorioAluno.js

--- src/Relatório/RelatorioAluno.js
+++ src/Relatório/RelatorioAluno.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { Teste } from "../Cadastrar/Cadastrar";
+import React, { useContext } from "react";
+import { AlunoContext } from "../AlunoContext/alunoContext";
+
 
 
 export default function RelatorioAluno(){
+    
+    const [alunos] = useContext(AlunoContext)
 
-       
     return(
-        <>
-            {Teste.map(aluno=>{
+        <>    
+            {alunos.map(aluno=>{
                 return(
                     <tr>
-                    <td> {Teste.indexOf(aluno)+1} </td>
+                    <td> {alunos.indexOf(aluno)+1} </td>
                     <td>{aluno}</td>
                     <td> <input type="number" name ="nota 1° Tri" placeholder="nota 1° Tri" className="notasR"/> </td>
                     <td> <input type="number" name ="nota 2° Tri" placeholder="nota 2° Tri" className="notasR"/> </td>
@@ -20,8 +22,7 @@ export default function RelatorioAluno(){
                     <td> <input type="number" name ="media final" placeholder="média final" className="notasR"/> </td>
                     </tr>
                 )
-                })}
-                    
+                })}          
         </>
     )
 }
