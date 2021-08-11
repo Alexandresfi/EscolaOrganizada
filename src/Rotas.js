5565b5175df3b8afbd4196aa6db7d81c9360b4c5
Alexandre Nascimento <81328399+Alexandresfi@users.noreply.github.com>
2021-08-11 14:06:39 -0300

    Update Rotas.js

--- src/Rotas.js
+++ src/Rotas.js
@@ -1,17 +1,20 @@
 import React from "react";
 import {Switch, Route} from "react-router-dom";
 import Login from './componentes/login/Login';
-import { Cadastrar } from './Cadastrar/Cadastrar';
+import Cadastrar from './Cadastrar/Cadastrar';
 import Fre from './Fre/Frquencia';
 import Faltas from "./Faltas/Faltas";
 import Notas from './Notas/Notas';
 import Relatorio from './Relatório/Relatorio';
 import Menu from "./Menu/Menu";
+import { AlunoProvider } from "./AlunoContext/alunoContext";
 
 export default function Rotas (){
     
     return(
         <Switch>
+
+            <AlunoProvider>
 
             <Route exact path="/">
                 <Login Titulo="Escola Organizada Sistema de Ensino" Subtitulo="Relatório Escolar" SI="Esqueceu a senha? Entre em contato com a secretária"/>
@@ -40,6 +43,8 @@ export default function Rotas (){
             <Route exact path="/relatorio">
                 <Relatorio/>
             </Route>
+            </AlunoProvider>
+
         </Switch>
     )
 
