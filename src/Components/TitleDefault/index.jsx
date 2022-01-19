93feccfd951916669a8e7d3d26fe5d72d4b69e66
Alexandre Nascimento <81328399+Alexandresfi@users.noreply.github.com>
2022-01-19 13:31:08 -0300

    just date

--- src/Components/TitleDefault/index.jsx
+++ src/Components/TitleDefault/index.jsx
@@ -18,6 +18,9 @@ export function TitleDefaults() {
             </Content>
 
             <Content>
+                {/* <li>
+                    { new Intl.DateTimeFormat('pt-Br').format( new Date())}
+                </li> */}
                 <li>
                     <a href="https://www.linkedin.com/in/alexandre-nascimento-66692920a/">
                         <img src={LinkdinImg} alt='Linkedin' />
