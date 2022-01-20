fd7cbf7479036dcdcf16a810987f781d7abdaad3
Alexandre Nascimento <81328399+Alexandresfi@users.noreply.github.com>
2022-01-20 12:18:10 -0300

    settings components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
-import {Rotas} from './Routes';
+import { BrowserRouter } from "react-router-dom";
+import { Rotas } from './Routes';
 import { GlobalStyle } from './Components/global';
 
 function App() {
-  
+
   return (
     <BrowserRouter>
       <Rotas />
-      <GlobalStyle/>
+      <GlobalStyle />
     </BrowserRouter>
-    
+
   );
 }
 
--- src/Components/TitleDefault/index.jsx
+++ src/Components/TitleDefault/index.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import { Container, Content } from "./styles";
 import LinkdinImg from "../../assects/Linkedin.png"
 import GithubImg from "../../assects/Github.png"
+import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { ButtonHearder } from "../Header/styled";
 
 
 export function TitleDefaults() {
-    
+    const localidation = useLocation()
+    const history = useHistory()
+
+    const [title, setTitle] = React.useState([])
+
+    useEffect(() => {
+        setTitle([...title, localidation.pathname.split('/')[1]])
+        console.log(title)
+        localidation.pathname === '/home' && setTitle([])
+    }, [localidation.pathname])
+
+
+
     return (
         <Container>
             <Content>
@@ -16,27 +32,44 @@ export function TitleDefaults() {
                 <li>
                     <h3>Alexandre Nascimento</h3>
                 </li>
+
+                <li>
+                    {localidation.pathname !== '/home' && (<p>| Você está aqui: </p>)}
+                </li>
+
+                <li>
+                    {localidation.pathname !== '/home' && (
+                        <>
+                            {title?.map((item, index) => (
+                                <span key={index}> {item} </span>
+                            ))}
+                        </>
+                    )}
+                </li>
             </Content>
 
             <Content>
-                {/* <li>
-                    { new Intl.DateTimeFormat('pt-Br').format( new Date())}
-                </li> */}
                 <li>
-                    <a href="https://www.linkedin.com/in/alexandre-nascimento-66692920a/">
-                        <img src={LinkdinImg} alt='Linkedin' />
-                    </a>
+                    {localidation.pathname === '/home' && (
+                        <a href="https://www.linkedin.com/in/alexandre-nascimento-66692920a/">
+                            <img src={LinkdinImg} alt='Linkedin' />
+                        </a>
+                    )}
 
                 </li>
 
                 <li>
-                    <a href="https://github.com/Alexandresfi">
-                        <img src={GithubImg} alt='Linkedin' />
-                    </a>
+                    {localidation.pathname === '/home' && (
+                        <a href="https://github.com/Alexandresfi">
+                            <img src={GithubImg} alt='Linkedin' />
+                        </a>
+                    )}
 
                 </li>
 
             </Content>
+            {localidation.pathname !== '/home' && <ButtonHearder onClick={()=> history.goBack()}> Voltar</ButtonHearder>}
+            
         </Container>
     )
 }
